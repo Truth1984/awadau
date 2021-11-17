5b6cd6fb3a10d90eb8954a1d9c3f7445ea125e0a
truth1984 <zhengyao365@163.com>
2021-11-17 14:33:15 +0800

    update dep and webpack, fix fetch

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,17 @@
 const path = require("path");
+const fs = require("fs");
+const u = require("./index");
+
+let fetchModPath = "./node_modules/isomorphic-fetch/fetch-npm-browserify.js";
+if (fs.existsSync(fetchModPath)) {
+  let content = fs.readFileSync(fetchModPath, "utf8").toString();
+  if (u.contains(content, "self.fetch.bind")) {
+    let replacement =
+      'var globalObject = typeof self === "undefined" ? global : self; module.exports = globalObject.fetch.bind(globalObject);';
+    content = content.replace("module.exports = self.fetch.bind(self);", replacement);
+    fs.writeFileSync(fetchModPath, content);
+  }
+}
 
 module.exports = {
   entry: "./entry.js",
